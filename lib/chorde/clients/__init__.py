31de92b9f4b25052b769b4b9bcbc7fbc67477948
Claudio Freire <klaussfreire@gmail.com>
2013-05-30 17:38:28 -0300

    Fix typo

--- lib/chorde/clients/__init__.py
+++ lib/chorde/clients/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .inproc import InprocCacheClient # before base, really, it injects CacheMissError
 from .base import BaseCacheClient, ReadWriteSyncAdapter, NONE, CacheMissError
-from .async import AsyncWriteCacheClient, Deferk, AsyncCacheProcessor
+from .async import AsyncWriteCacheClient, Defer, AsyncCacheProcessor
 from .tiered import TieredInclusiveClient
 from .coherent import CoherentDefer, CoherentWrapperClient
 
