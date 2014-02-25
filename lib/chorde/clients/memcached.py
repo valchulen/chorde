d6610ffc095431aec9f537a91ba32bc4586ac037
Claudio Freire <klaussfreire@gmail.com>
2014-02-25 20:37:15 -0300

    Add support for an encoding (pickling) cache for MemcachedClient, to speed up multi-tier promotion and insertion (avoid pickling multiple times)

--- lib/chorde/clients/memcached.py
+++ lib/chorde/clients/memcached.py
@@ -219,6 +219,7 @@ class MemcachedClient(DynamicResolvingMemcachedClient):
             pickler = None,
             namespace = None,
             checksum_key = None, # CHANGE IT!
+            encoding_cache = None, # should be able to contain attributes
             **client_args):
         if checksum_key is None:
             raise ValueError, "MemcachedClient requires a checksum key for security checks"
@@ -237,6 +238,7 @@ class MemcachedClient(DynamicResolvingMemcachedClient):
         self.namespace = namespace
         self.failfast_time = failfast_time
         self.succeedfast_time = succeedfast_time
+        self.encoding_cache = encoding_cache
         
         if self.namespace:
             self.max_backing_key_length -= len(self.namespace)+1
@@ -357,22 +359,30 @@ class MemcachedClient(DynamicResolvingMemcachedClient):
         return stamp
     
     def encode_pages(self, key, ttl, value):
-        # Always pickle & compress, since we'll always unpickle.
-        # Note: compress with very little effort (level=1), 
-        #   otherwise it's too expensive and not worth it
-        sio = StringIO()
-        with ZlibFile(sio, 1) as zio:
-            self.pickler.dump((key,value),zio,2)
-        value = sio.getvalue()
-        sio.close()
-        del sio,zio
+        encoded = None
+        if self.encoding_cache is not None:
+            cached = getattr(self.encoding_cache, 'cache', None)
+            if cached is not None and cached[0] is value:
+                encoded = cached[1]
+            del cached
+
+        if encoded is None:
+            # Always pickle & compress, since we'll always unpickle.
+            # Note: compress with very little effort (level=1), 
+            #   otherwise it's too expensive and not worth it
+            sio = StringIO()
+            with ZlibFile(sio, 1) as zio:
+                self.pickler.dump((key,value),zio,2)
+            encoded = sio.getvalue()
+            sio.close()
+            del sio,zio
         
-        npages = (len(value) + self.max_backing_value_length - 1) / self.max_backing_value_length
+        npages = (len(encoded) + self.max_backing_value_length - 1) / self.max_backing_value_length
         pagelen = self.max_backing_value_length
         version = self.get_version_stamp()
         page = 0
-        for page,start in enumerate(xrange(0,len(value),self.max_backing_value_length)):
-            yield (npages, page, ttl, version, value[start:start+pagelen])
+        for page,start in enumerate(xrange(0,len(encoded),self.max_backing_value_length)):
+            yield (npages, page, ttl, version, encoded[start:start+pagelen])
         
         assert page == npages-1
 
@@ -400,10 +410,13 @@ class MemcachedClient(DynamicResolvingMemcachedClient):
         
         # join pages, decompress, unpickle
         data = ''.join(data)
-        data = zlib.decompress(data)
-        data = self.pickler.loads(data)
+        value = zlib.decompress(data)
+        value = self.pickler.loads(value)
+
+        if self.encoding_cache is not None:
+            self.encoding_cache.cache = (value, data)
         
-        return data
+        return value
     
     def _getTtl(self, key, default, decode = True, ttl_skip = None, short_key = None, pages = None):
         now = time.time()
