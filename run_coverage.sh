509a005d49f89ed4b05e2ae030850f6a90d7c15b
Claudio Freire <klaussfreire@gmail.com>
2014-05-12 17:41:49 -0300

    Script to do coverage

---
+++ run_coverage.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm ./coverage/*.html ./coverage/*.js ./coverage/*.css ./coverage/keybd_*.png
+nosetests-2.7 -v --with-coverage --cover-erase --cover-html --cover-package=chorde --cover-html-dir=./coverage tests/*
+
