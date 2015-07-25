11461e88ea0c989ef6153b674bfb6c0c1a4c0944
Claudio Freire <klaussfreire@gmail.com>
2015-07-24 22:07:27 -0300

    Add xml and proper error checking to run_coverage.sh

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -e
 rm -f ./coverage/*.html ./coverage/*.js ./coverage/*.css ./coverage/keybd_*.png ./coverage/status.dat
 rm -f .coverage
 coverage-2.7 run --branch setup.py test $@
 coverage-2.7 report $(find lib/chorde -name '*.py')
 coverage-2.7 html -d ./coverage $(find lib/chorde -name '*.py')
+coverage-2.7 xml -o coverage.xml $(find lib/chorde -name '*.py')
 
