2fe422559f408b0bf7d14bbc53f63cba0e88fe6d
Claudio Freire <klaussfreire@gmail.com>
2017-08-23 12:27:31 -0300

    More tests

--- tests/pycuckoocache.py
+++ tests/pycuckoocache.py
@@ -28,6 +28,18 @@ class PyCuckooCacheTest(unittest.TestCase):
         for k,v in self.TEST_ELEMENTS:
             self.assertEqual(c.get(k, overflow.get(k)), v)
 
+    def testNoCallback(self):
+        c = self.Cache(20)
+        for k,v in self.TEST_ELEMENTS:
+            c[k] = v
+
+        self.assertLessEqual(len(c), len(self.TEST_ELEMENTS))
+        for k,v in self.TEST_ELEMENTS:
+            if k in c:
+                self.assertEqual(c.get(k), v)
+                self.assertEqual(c[k], v)
+                self.assertIs(c.setdefault(k, None), v)
+
     def testContains(self, **kwargs):
         evictions = []
         eviction_callback = lambda k,v : evictions.append((k,v))
@@ -44,11 +56,60 @@ class PyCuckooCacheTest(unittest.TestCase):
         for k,v in self.TEST_ELEMENTS:
             self.assertTrue(k in c or k in overflow)
 
+    def testSetDefault(self, **kwargs):
+        evictions = []
+        eviction_callback = lambda k,v : evictions.append((k,v))
+        c = self.Cache(20, eviction_callback = eviction_callback, **kwargs)
+
+        for k,v in self.TEST_ELEMENTS:
+            # Construct a distinct object
+            v = float(v)
+            self.assertIs(c.setdefault(k,v), v)
+
+            # Second time returns the first one
+            v2 = float(v)
+            self.assertIs(c.setdefault(k,v2), v)
+
+    def testCas(self, **kwargs):
+        evictions = []
+        eviction_callback = lambda k,v : evictions.append((k,v))
+        c = self.Cache(20, eviction_callback = eviction_callback, **kwargs)
+
+        for k,v in self.TEST_ELEMENTS:
+            v2 = float(v)
+            c[k] = v2
+            c.cas(k,v2,v)
+            self.assertIs(c[k], v)
+
+    def testPop(self, **kwargs):
+        evictions = []
+        eviction_callback = lambda k,v : evictions.append((k,v))
+        c = self.Cache(20, eviction_callback = eviction_callback, **kwargs)
+
+        for k,v in self.TEST_ELEMENTS:
+            self.assertRaises(pycuckoocache.CacheMissError, c.pop, k)
+            self.assertIs(c.pop(k, None), None)
+            c[k] = v
+            self.assertIs(c.pop(k), v)
+            self.assertRaises(pycuckoocache.CacheMissError, c.pop, k)
+            self.assertIs(c.pop(k, None), None)
+
     def testRehash(self):
         self.testAdd(initial_size = 1)
 
     def testPreallocate(self):
         self.testAdd(preallocate = True)
+
+    def testClear(self):
+        c = self.Cache(20)
+        for k,v in self.TEST_ELEMENTS:
+            c[k] = v
+        c.clear()
+        self.assertEqual(len(c), 0)
+        for k,v in self.TEST_ELEMENTS:
+            c[k] = v
+
+        self.assertLessEqual(len(c), len(self.TEST_ELEMENTS))
 
     def testRepriorize(self):
         evictions = []
